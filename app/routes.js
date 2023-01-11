82aebf09b5871875c01a963bfa635234027bf7e2
Anthony O'Malley <anthony@dxw.com>
2023-01-11 15:18:19 +0000

    Feat: tidy and fix up sprint 20 feedback form

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,21 @@
 // https://prototype-kit.service.gov.uk/docs/routes
 // 
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
-// Add your routes here
+// Sprint 20 - Feedback
+
+router.post('/20/feedback/start', function (req, res) {
+
+  var howCanWeHelp = req.session.data['how-can-we-help']
+
+  if (howCanWeHelp == "I want to report a problem with this form") {
+    res.redirect("/20/feedback/problem");
+  } else if (howCanWeHelp == "I need help with an application") {
+    res.redirect("/20/feedback/help");
+  } else {
+    res.redirect("/20/feedback/feedback");
+  }
+
+})
