b66e6b9f8e978841e9436a2bf87517cdb924d5ff
Anthony O'Malley <anthony@dxw.com>
2023-01-13 10:27:41 +0000

    Add back links to sprint 20 feedback
    It makes sense to allow the user to get back to where they came from, especially if they arrived in the section by accident.

--- app/routes/20/feedback.js
+++ app/routes/20/feedback.js
@@ -2,7 +2,7 @@
 
 module.exports = function (router) {
 
-  router.post("/20/feedback/start", function (req, res) {
+  router.post("/20/feedback/how-can-we-help", function (req, res) {
     var howCanWeHelp = req.session.data["how-can-we-help"];
 
     if (howCanWeHelp == "I want to report a problem with this form") {
